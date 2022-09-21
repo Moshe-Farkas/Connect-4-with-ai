90f7e024ceed8099f3eeef832878ab1f0c2a64e9
natan_anter <natananter@gmail.com>
2022-09-21 17:45:08 +0300

    added all back end logic no AI yet

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,27 @@
 import game.*;
 import gui.*;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x;
+        int s;
         Game g = new Game();
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-        g.placePiece(4);
-
-
-        g.printBoard();
-
+        while (true){
+            System.out.print("play your move: ");
+            x = sc.nextInt();
+            s = g.play(x);
+            System.out.println();
+            if(s == 1){
+                System.out.println("first player won");
+                break;
+            }
+            if(s == 2){
+                System.out.println("second player won");
+                break;
+            }
+        }
     }
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -7,14 +7,12 @@ public class Game {
     public int[][] board;          // 0 == empty space. 1 == taken by player one. 2 == taken by player 2
     public int lastColumn;
     public int lastRow;
-
     public Game() {
-        board = new int[6][7];
+        board = new int[10][7];
         InitBoard();
         Random rand = new Random();
         isFirstPlayerTurn = rand.nextInt() % 2 == 0;       // chooses random player to start
     }
-
     public void InitBoard() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
@@ -22,8 +20,21 @@ public class Game {
             }
         }
     }
+    public int play(int column){
+        lastColumn = column;
+        if(!placePiece())
+            return -1;
+        printBoard();
+        int winner = CheckForWinner();
+        isFirstPlayerTurn = !isFirstPlayerTurn;
+        return winner;
+    }
 
     public void printBoard() {
+        if(isFirstPlayerTurn)
+            System.out.println("first player turn ");
+        else
+            System.out.println("second player turn ");
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 System.out.print(board[i][j] + ", ");
@@ -32,24 +43,21 @@ public class Game {
         }
     }
 
-    public boolean placePiece(int column) {
+    public boolean placePiece() {
         int rowPointer = 0;
         while (true) {
-            if (board[0][column] != 0) {
+            if (board[0][lastColumn] != 0) {
                 return false;
             }
-            lastColumn = column;
             if (rowPointer == board.length) {
-                board[rowPointer - 1][column] = getTurnVal();
+                board[rowPointer - 1][lastColumn] = getTurnVal();
                 lastRow = rowPointer - 1;
                 return true;
             }
-
-            if (board[rowPointer][column] != 0) {
-                board[rowPointer - 1][column] = getTurnVal();
+            if (board[rowPointer][lastColumn] != 0) {
+                board[rowPointer - 1][lastColumn] = getTurnVal();
                 lastRow = rowPointer - 1;
                 return true;
-
             }
             rowPointer++;
         }
@@ -58,7 +66,6 @@ public class Game {
     private void SwitchPlayer() {
         isFirstPlayerTurn = !isFirstPlayerTurn;
     }
-
     private int getTurnVal() {
         if (isFirstPlayerTurn) {
             return 1;
@@ -66,15 +73,61 @@ public class Game {
             return 2;
         }
     }
-
-    private int CheckForWinner(int column) {
-        int winner = checkVertically();
+    private int CheckForWinner() {
+        int winner = CheckVertically();
+        if (winner != 0)
+            return winner;
+        winner = CheckHorizontally();
+        if (winner != 0)
+            return winner;
+        winner = CheckDiagonally1();
+        if (winner != 0)
+            return winner;
+        winner = CheckDiagonally2();
         if (winner != 0)
             return winner;
         return 0;
     }
+    private int CheckDiagonally1(){
+        int FirstColumn = Math.max(0,lastRow+lastColumn-(board.length-1));
+        int FirstRow = lastRow + lastColumn - FirstColumn;
+        int numOfSquares = Math.min(Math.min(board.length-1,board[0].length-1) ,Math.min(lastRow + lastColumn,board.length - 1 + board[0].length - 1 - lastColumn -lastRow)+1);
 
-    private int CheckHorizantlly() {
+        int target = getTurnVal();
+        int counter = 0;
+        int row = FirstRow;
+        for (int column = FirstColumn; column < numOfSquares; column++){
+            if (board[row][column] == target)
+                counter++;
+            else
+                counter = 0;
+            if (counter == 4)
+                return target;
+            row--;
+        }
+        return 0;
+    }
+    private int CheckDiagonally2(){
+        int FirstColumn = Math.max(0, lastColumn - lastRow);
+        int FirstRow = lastRow - lastColumn + FirstColumn;
+        int numOfSquares = Math.min(Math.min(board.length - 1,board[0].length - 1) ,Math.min(lastRow + board[0].length - 1 - lastRow,  lastColumn + board.length - 1 -lastRow)+1);
+
+        int target = getTurnVal();
+        int counter = 0;
+        int row = FirstRow;
+        for (int column = FirstColumn; column < numOfSquares; column++){
+            if (board[row][column] == target)
+                counter++;
+            else
+                counter = 0;
+            if (counter == 4)
+                return target;
+            row++;
+        }
+        return 0;
+    }
+
+    private int CheckHorizontally() {
         int target = getTurnVal();
         int counter = 0;
         for (int i = 0; i < board[0].length; i++) {
@@ -89,7 +142,7 @@ public class Game {
         return 0;
     }
 
-    private int checkVertically() {
+    private int CheckVertically() {
         int target = getTurnVal();
         int counter = 0;
         for(int i = 0; i < board.length; i++) {
@@ -100,12 +153,6 @@ public class Game {
             if (counter == 4)
                 return target;
         }
-        return 0;
-    }
-    private int checkDiagnally(){
-
-
-
         return 0;
     }
 
